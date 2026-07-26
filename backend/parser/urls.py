412fb07ff73478089534eaf8fe0890c2e56cd344
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-26 17:49:19 +0530

    Add HistoryAPIView to URL patterns

--- backend/parser/urls.py
+++ backend/parser/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import FileUploadAPIView
+from .views import FileUploadAPIView, HistoryAPIView
 
 urlpatterns = [
     path("upload/", FileUploadAPIView.as_view(), name="upload-file"),
+    path("history/", HistoryAPIView.as_view(), name="history"),
 ]
