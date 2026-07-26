9b3f621b787e78e4013a0187df18c6a65006c4f2
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-26 16:50:59 +0530

    Modify ExecutionEvent model fields and defaults
    Updated ExecutionEvent model to include filename and set default values for line_number and serialized_value.

--- backend/parser/models.py
+++ backend/parser/models.py
@@ -2,13 +2,11 @@ from django.db import models
 
 # Create your models here.
 class ExecutionEvent(models.Model):
+    filename = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
-    line_number = models.IntegerField()
     variable_name = models.CharField(max_length=100)
-    serialized_value = models.TextField()
-
-    class Meta:
-        ordering = ["timestamp"]
+    line_number = models.IntegerField(default=0)
+    serialized_value = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.variable_name} @ Line {self.line_number}"
+        return f"{self.filename} - {self.variable_name}"
