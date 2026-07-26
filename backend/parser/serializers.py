381dad776d1f474b06cb4bc6177ed2954a5327d0
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-26 17:47:58 +0530

    Update serializers.py

--- backend/parser/serializers.py
+++ backend/parser/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
+from .models import ExecutionEvent
 
 
 class PythonFileSerializer(serializers.Serializer):
     file = serializers.FileField()
+
+
+class ExecutionEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExecutionEvent
+        fields = "__all__"
