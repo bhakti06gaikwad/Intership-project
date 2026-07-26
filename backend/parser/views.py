e57f00905f0f89e3c566bb46f07001c65d325b94
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-26 16:51:30 +0530

    Enhance FileUploadAPIView to analyze uploaded Python files
    The FileUploadAPIView now processes uploaded Python files, analyzes them for variables and functions using the AST parser, and logs execution events.

--- backend/parser/views.py
+++ backend/parser/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
+from .ast_parser import analyze_python_code
+from .models import ExecutionEvent
+
+
 class FileUploadAPIView(APIView):
 
     def post(self, request):
+
         uploaded_file = request.FILES.get("file")
 
         if not uploaded_file:
@@ -15,11 +18,23 @@ class FileUploadAPIView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        return Response(
-            {
-                "status": "success",
-                "filename": uploaded_file.name,
-                "size": uploaded_file.size,
-            }
-        )
+        code = uploaded_file.read().decode("utf-8")
 
+        result = analyze_python_code(code)
+
+        for variable in result["variables"]:
+            ExecutionEvent.objects.create(
+                filename=uploaded_file.name,
+                variable_name=variable,
+                line_number=0,
+                serialized_value="Detected by AST"
+            )
+
+        return Response({
+            "status": "success",
+            "filename": uploaded_file.name,
+            "variables": result["variables"],
+            "functions": result["functions"],
+            "variable_count": result["variable_count"],
+            "function_count": result["function_count"]
+        })
