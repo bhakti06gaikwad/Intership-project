ade7b859b7fb3c34b9ebf0c72de6782c60b3aa1c
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-26 16:49:20 +0530

    Update ast_parser.py

--- backend/parser/ast_parser.py
+++ backend/parser/ast_parser.py
@@ -1,92 +1,37 @@
-import os
 import ast
-import sys
-from pathlib import Path
-
-# Add backend directory to Python path
-BASE_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(BASE_DIR))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-import django
-
-django.setup()
-
-from parser.models import ExecutionEvent
 
 
 class VariableVisitor(ast.NodeVisitor):
 
     def __init__(self):
         self.variables = []
+        self.functions = []
 
     def visit_Assign(self, node):
-
         for target in node.targets:
-
-            # x = 10
             if isinstance(target, ast.Name):
-                self.variables.append(
-                    {
-                        "name": target.id,
-                        "line": node.lineno,
-                    }
-                )
+                self.variables.append({
+                    "name": target.id,
+                    "line": node.lineno
+                })
+        self.generic_visit(node)
 
-            # a, b = (1,2)
-            elif isinstance(target, ast.Tuple):
-
-                for element in target.elts:
-
-                    if isinstance(element, ast.Name):
-                        self.variables.append(
-                            {
-                                "name": element.id,
-                                "line": node.lineno,
-                            }
-                        )
-
+    def visit_FunctionDef(self, node):
+        self.functions.append(node.name)
         self.generic_visit(node)
 
 
-class ASTParser:
+def analyze_python_code(code):
 
-    def __init__(self, filepath):
-        self.filepath = Path(filepath)
+    tree = ast.parse(code)
 
-    def parse(self):
+    visitor = VariableVisitor()
 
-        source = self.filepath.read_text(encoding="utf-8")
+    visitor.visit(tree)
 
-        tree = ast.parse(source)
-
-        visitor = VariableVisitor()
-        visitor.visit(tree)
-
-        return visitor.variables
-
-
-def save_to_database(variables):
-
-    for variable in variables:
-
-        ExecutionEvent.objects.create(
-            line_number=variable["line"],
-            variable_name=variable["name"],
-            serialized_value="None"
-        )
-
-
-if __name__ == "__main__":
-
-    parser = ASTParser("examples/simple.py")
-
-    variables = parser.parse()
-
-    save_to_database(variables)
-
-    print("\nVariables Stored Successfully\n")
-
-    for item in variables:
-        print(item)
+    return {
+        "variables": visitor.variables,
+        "functions": visitor.functions,
+        "variable_count": len(visitor.variables),
+        "function_count": len(visitor.functions),
+    }
