6c6b188af6e7d640061f74af67cf128cb7026fec
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-27 19:44:41 +0530

    Refactor Navbar component to use Links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <header className="navbar">
+    <nav>
       <h2>PyChronicle</h2>
 
       <div>
-        <span>Developer Dashboard</span>
+        <Link to="/">Dashboard</Link> {" | "}
+        <Link to="/history">History</Link> {" | "}
+        <Link to="/timeline">Timeline</Link> {" | "}
+        <Link to="/variables">Variables</Link>
       </div>
-    </header>
+    </nav>
   );
 }
 
