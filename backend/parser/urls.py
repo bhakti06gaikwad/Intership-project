eba42e552c712fb5b01e067195d567314fcf193d
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-27 19:00:44 +0530

    Add TimelineAPIView to URL patterns

--- backend/parser/urls.py
+++ backend/parser/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FileUploadAPIView, HistoryAPIView
+from .views import FileUploadAPIView, HistoryAPIView ,TimelineAPIView
 
 urlpatterns = [
     path("upload/", FileUploadAPIView.as_view(), name="upload-file"),
     path("history/", HistoryAPIView.as_view(), name="history"),
+    path("timeline/", TimelineAPIView.as_view(), name="timeline"),
 ]
