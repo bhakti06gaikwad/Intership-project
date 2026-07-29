dff05cfca9c6fd74e43ed189af54f2269115cca7
bhakti06gaikwad <bhaktisgaikwad2007@gmail.com>
2026-07-29 22:19:03 +0530

    Update models.py

--- backend/parser/models.py
+++ backend/parser/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
-# Create your models here.
 class ExecutionEvent(models.Model):
+
     filename = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
+
     variable_name = models.CharField(max_length=100)
-    line_number = models.IntegerField(default=0)
-    serialized_value = models.TextField(blank=True)
+
+    serialized_value = models.TextField()
+
+    line_number = models.IntegerField()
+
+    function_name = models.CharField(
+        max_length=100,
+        blank=True,
+        default=""
+    )
+
+    execution_time = models.FloatField(default=0)
+
+    event_type = models.CharField(
+        max_length=50,
+        default="variable"
+    )
+
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.filename} - {self.variable_name}"
